467c0bfc395caea086a2fe83d2b180d748d63e51
yeong-hwan <jghff700@naver.com>
2024-01-31 13:30:25 +0900

    test: send message using discord alert bot

---
+++ test/discord_alert_test.py
@@ -0,0 +1,6 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from alert import DiscordAlertBot
+import time
+
+DiscordAlertBot.send_discord_alert("sening test")
