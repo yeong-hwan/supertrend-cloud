85f34d89c4a03f1b212f937d587d95188a7df7a5
yeong-hwan <jghff700@naver.com>
2024-01-31 13:21:43 +0900

    feat(alert): add discord message alert bot

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # supertrend-cloud
 Coin trading bot using supertrend-cloud strategy
+
+### private key
+Server
+- ASW EC2 pem key
+Alert
+- Discord Channel ID
+- Discord Bot Token
+
+### Libraries
+- [Discord](https://discordpy.readthedocs.io/en/stable/)
---
+++ alert/DiscordAlertBot.py
@@ -0,0 +1,32 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from private import private_key
+
+import discord
+from discord.ext import commands
+
+CHANNEL_ID = private_key.DISCORD['CHANNEL_ID']
+TOKEN = private_key.DISCORD['BOT_TOKEN']
+
+class DiscordAlertBot(discord.Client):
+    message = "Default Message"
+
+    def set_message(self, message):
+        self.message = message
+
+    async def on_ready(self):
+        channel = self.get_channel(int(CHANNEL_ID))
+        await channel.send(self.message)
+        await self.close()
+
+    # async def on_message(self, message):
+    #     # contents
+    #     return
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+def send_discord_alert(message):
+    discord_alert_bot = DiscordAlertBot(intents=intents)
+    discord_alert_bot.set_message(message)
+    discord_alert_bot.run(TOKEN)
