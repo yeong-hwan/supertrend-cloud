6f0b0d95bdfef7aa0a5c7f90761765c040f12653
yeong-hwan <jghff700@naver.com>
2024-02-09 16:01:40 +0900

    feat: implement binance class

---
+++ binance/Binance.py
@@ -0,0 +1,30 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from private import private_key
+import RsaEnDecrypt
+
+import ccxt
+
+class Binance:
+    def __init__(self):
+        rsa_decrypt = RsaEnDecrypt.RsaEnDecrypt(private_key.RSA['ENCRYPT_KEY'])
+        binance_access = rsa_decrypt.decrypt(private_key.RSA['ACCESS'])
+        binance_secret = rsa_decrypt.decrypt(private_key.RSA['SECRET'])
+
+        self.binance = ccxt.binance(config={
+            'apiKey': binance_access,
+            'secret': binance_secret,
+            'enableRateLimit': True,
+            'options': {
+                'defaultType': 'future'
+            }
+        })
+
+        self.balance = self.binance.fetch_balance(params={"type": "future"})
+
+    def get_balance(self):
+        print(self.balance)
+        return self.balance
+
+# binance = Binance()
+# print(binance.get_balance())
