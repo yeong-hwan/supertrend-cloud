5344eb014edb37fe963f6284e15bb9ace8c1113f
yeong-hwan <jghff700@naver.com>
2024-02-09 18:14:30 +0900

    refactor(constants): add local & server path constant

--- constants/constants.py
+++ constants/constants.py
@@ -2,3 +2,12 @@ BINANCE = {
     "LEVERAGE": 3,
     "IS_ISOLATED": True
 }
+
+PATH = {
+    # "SERVER": "/var/trading-bot/positioned_list.json",
+    "LOCAL": "log/positioned_list.json"
+}
+
+TICKER = {
+    "BANNED": ["SRM/USDT", "FTT/USDT"]
+}
