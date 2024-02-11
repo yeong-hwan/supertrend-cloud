49fc5db569ce1b5ce798a89c1f68677a372f5f10
yeong-hwan <jghff700@naver.com>
2024-02-12 00:42:21 +0900

    refactor(constants): add setting constant for supertrend

--- constants/constants.py
+++ constants/constants.py
@@ -9,6 +9,36 @@ PATH = {
 }
 
 TICKER = {
-    "BANNED": ["SRM/USDT", "FTT/USDT"],
+    "BANNED": [
+        "SRM/USDT",
+        "FTT/USDT"
+    ],
     "TICKER_COUNT": 10
 }
+
+STATE = {
+    "CLOUD"
+}
+
+SUPERTREND_SETTING = {
+    "5M": {
+        "PERIOD_1": 6,
+        "MULTIPLIER_1": 10,
+        "PERIOD_2": 10,
+        "MULTIPLIER_2": 6
+    },
+    "4H": {
+        "PERIOD_1": 10,
+        "MULTIPLIER_1": 3,
+        "PERIOD_2": 10,
+        "MULTIPLIER_2": 6
+    },
+    "BTC_4H": {
+        "PERIOD_1": 4,
+        "MULTIPLIER_1": 2.4,
+        "PERIOD_2": 4,
+        "MULTIPLIER_2": 4.8
+    }
+}
+
+# print(SUPERTREND_SETTING['5M']['PERIOD'])
