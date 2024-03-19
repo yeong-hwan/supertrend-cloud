70a26a2da92541f52a1617e25e27f7aaa93449df
yeong-hwan <jghff700@naver.com>
2024-03-19 11:01:07 +0900

    refactor(rsa): move rsa class to util package

--- binance/Binance.py
+++ binance/Binance.py
@@ -3,7 +3,7 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from private import private_key
 from constants import constants
-import RsaEnDecrypt
+from util import RsaEnDecrypt
 
 import ccxt
 import pandas as pd
